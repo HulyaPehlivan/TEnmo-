792da03766f774ba2e7d95b6ef56208302bec849
Sean Kunkemoeller <seankunke98@gmail.com>
2022-08-24 11:12:34 -0400

    capstone 2 initial changes

---
+++ tenmo/server/src/main/java/com/techelevator/tenmo/controller/AccountController.java
@@ -0,0 +1,45 @@
+package com.techelevator.tenmo.controller;
+
+
+import com.techelevator.tenmo.dao.AccountDao;
+import com.techelevator.tenmo.dao.JdbcAccountDao;
+import com.techelevator.tenmo.model.Account;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.lang.annotation.Repeatable;
+import java.math.BigDecimal;
+import java.util.List;
+
+@RestController
+@PreAuthorize("isAuthorized()")
+public class AccountController {
+
+    private AccountDao accountDao;
+
+    public AccountController(AccountDao accountDao){
+        this.accountDao = accountDao;
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @RequestMapping(path = "/accounts", method = RequestMethod.GET)
+    public List<Account> getAllAccounts(){
+        List<Account> accounts = accountDao.getAllAccounts();
+        return accounts;
+    }
+
+    @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
+    @RequestMapping(path = "/accounts/{id}", method = RequestMethod.GET)
+    public Account getAccountByUserId(@PathVariable int id){
+        Account account = accountDao.getAccountById(id);
+        return account;
+    }
+
+    @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
+    @RequestMapping(path = "/accounts/transfer", method = RequestMethod.PUT)
+    public void transferMoney(@RequestParam(name = "account_from") int fromId, @RequestParam(name = "account_to") int toId, @RequestParam(name = "amount") BigDecimal amount){
+        accountDao.addBalance(toId, amount);
+        accountDao.subtractBalance(fromId, amount);
+    }
+
+}
---
+++ tenmo/server/src/main/java/com/techelevator/tenmo/dao/JdbcAccountDao.java
@@ -0,0 +1,85 @@
+package com.techelevator.tenmo.dao;
+
+import com.techelevator.tenmo.model.Account;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class JdbcAccountDao implements AccountDao {
+
+    private JdbcTemplate jdbcTemplate;
+
+    public JdbcAccountDao(DataSource dataSource){
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    @Override
+    public BigDecimal getAccountBalance(int id) {
+        BigDecimal balance = null;
+        String sql = "SELECT balance FROM account WHERE account_id = ?";
+        SqlRowSet result = jdbcTemplate.queryForRowSet(sql, id);
+        if(result.next()){
+            Account account = mapRowToAccount(result);
+            balance = account.getBalance();
+        }
+        return balance;
+    }
+
+    @Override
+    public List<Account> getAllAccounts() {
+        List<Account> accounts = new ArrayList<>();
+        String sql = "SELECT * FROM account";
+        SqlRowSet result = jdbcTemplate.queryForRowSet(sql);
+        while(result.next()){
+            accounts.add(mapRowToAccount(result));
+        }
+        return accounts;
+    }
+
+    @Override
+    public Account getAccountById(int id) {
+        Account account = new Account();
+        String sql = "SELECT * FROM account WHERE user_id = ?";
+        SqlRowSet result = jdbcTemplate.queryForRowSet(sql, id);
+        if(result.next()){
+           account = mapRowToAccount(result);
+        }
+        return account;
+    }
+
+    @Override
+    public BigDecimal addBalance(int id, BigDecimal amount) {
+        String sql = "UPDATE account SET balance = (balance + ?) WHERE user_id = ?";
+        jdbcTemplate.update(sql, amount, id);
+        return getAccountBalance(id);
+    }
+
+    @Override
+    public BigDecimal subtractBalance(int id, BigDecimal amount) {
+        return null;
+    }
+
+    @Override
+    public Account getAccountByUserId(int id) {
+        return null;
+    }
+
+    @Override
+    public boolean checkValidTransfer(int id, BigDecimal balance) {
+        return false;
+    }
+    
+    private Account mapRowToAccount(SqlRowSet result){
+        Account account = new Account();
+        account.setAccountId(result.getInt("account_id"));
+        account.setUserId(result.getInt("user_id"));
+        account.setBalance(result.getBigDecimal("balance"));
+        return account;
+    }
+}
